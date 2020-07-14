0d7899657f79dc6145c13863564f2e11fc2d6017
drchico <github@drchico.net>
2020-07-14 20:58:58 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -83,61 +83,21 @@ class ServiceNowAdapter extends EventEmitter {
     this.healthcheck();
   }
 
-/**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
-log.error("IN HEALTHCHECK : Adapter ID : " + this.id);
-//this.emitOnline();
-
- this.getRecord((data, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-    log.error ("IN THIS.GETRECORD : Adapter ID : " + this.id);
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-      log.error("ERROR LOG : Adapter ID : " + this.id);
-      this.emitOnline();
-
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      log.error("passed healthcheck : Adapter ID : " + this.id);
-      this.emitOnline();
-   }
- });
-}
- 
+  /**
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -192,18 +152,13 @@ log.error("IN HEALTHCHECK : Adapter ID : " + this.id);
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-     log.error("IN GETRECORD : Adapater ID : " + this.id);
-    this.connector.get((data, error) => {
+    connector.get((data, error) => {
     if (error) {
-      log.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-    //callback.error = error;
+      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
     }
-    //callback.result = result;
-    //log.error(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-    return callback(data,error);
+    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
   });
-    log.error("OUT GETRECORD : Adapter ID : " + this.id);
-    //return callback(data, error);
+
   }
 
   /**
@@ -222,7 +177,7 @@ log.error("IN HEALTHCHECK : Adapter ID : " + this.id);
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-  this.connector.post((data, error) => {
+  connector.post((data, error) => {
     if (error) {
       console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
     }
